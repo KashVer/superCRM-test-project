aa2ec020a6f9166f2c4f44d55baa7bb98683c8b8
KashVer <veronika.kashkadova55@gmail.com>
2023-09-02 11:48:17 +0300

    change css

--- src/components/ToDoList.tsx
+++ src/components/ToDoList.tsx
@@ -9,58 +9,51 @@ interface IToDoListProps {
 
 export const ToDoList: React.FC<IToDoListProps> = ({ toDos }) => {
   return (
-    <ul className="flex flex-col gap-[8px]">
+    <ul className="flex flex-col gap-[8px] overflow-y-scroll">
       {toDos &&
         toDos.map((toDo: any) => {
           return (
             <li
               key={`${toDo.id}${toDo.title}`}
-              className="flex flex-col gap-[8px] p-[10px] rounded-[7px] bg-white shadow shadow-[0px 1px 2px 0px #D0DAEB, 0px 0px 2px 0px #EEF4FE]"
+              className="p-[4px] rounded-[7px] bg-[#F4F8FF]"
+              // style={{
+              //   boxShadow: "0px 1px 2px 0px #D0DAEB, 0px 0px 2px 0px #EEF4FE",
+              // }}
             >
-              <div className="flex gap-[8px] items-baseline">
-                <input
-                  type="checkbox"
-                  readOnly
-                  checked={toDo.completed}
-                  value=""
-                ></input>
-                <label className="text-[#3D8FEC] line-clamp-3 font-semibold text-sm">
-                  {toDo.title}
-                </label>
-              </div>
-              <div className="flex flex-col gap-[4px]">
-                <div className="flex justify-between gap-[8px] text-[#50B810] text-[13px] font-semibold leading-[18px]">
-                  <p>{formattedDateOne}</p>
-                  <p>{formattedDateTwo}</p>
+              <div className="bg-white flex flex-col gap-[8px] p-[10px] rounded-[7px] shadow shadow-[0px 1px 2px 0px #D0DAEB, 0px 0px 2px 0px #EEF4FE]">
+                <div className="flex gap-[8px] items-baseline">
+                  <input
+                    type="checkbox"
+                    readOnly
+                    checked={toDo.completed}
+                    value=""
+                  ></input>
+                  <label className="text-[#3D8FEC] line-clamp-3 font-semibold text-sm">
+                    {toDo.title}
+                  </label>
                 </div>
-                <p className="font-normal text-[13px] leading-[18px] line-clamp-1 text-[#555555]">
-                  {randomDescription}
-                </p>
-              </div>
-              <div className="flex justify-between items-center">
-                <div className="flex h-[20px]">
-                  <span className="flex justify-center items-center gap-[8px] bg-[#B233A6] px-[6px] py-[2px] mr-[4px] rounded-[4px] text-white font-semibold text-xs">
-                    {randomTagOne}
-                  </span>
-                  <div className="flex justify-center items-center w-[72px] px-[6px] py-[2px] bg-[#EBEEF6] rounded-[4px]">
-                    <span className="flex justify-center items-center text-[#616C82] font-semibold text-xs h-[20px]">
-                      {randomTagTwo}
-                    </span>
+                <div className="flex flex-col gap-[4px]">
+                  <div className="flex justify-between gap-[8px] text-[#50B810] text-[13px] font-semibold leading-[18px]">
+                    <p>{formattedDateOne}</p>
+                    <p>{formattedDateTwo}</p>
                   </div>
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="11"
-                    height="20"
-                    viewBox="0 0 11 20"
-                    fill="none"
-                  >
-                    <path
-                      d="M0 0H2.0775C3.29264 0 4.44189 0.552359 5.20098 1.50122L10.001 7.50122C11.1697 8.96209 11.1697 11.0379 10.001 12.4988L5.20098 18.4988C4.44189 19.4476 3.29264 20 2.0775 20H0V0Z"
-                      fill="#EBEEF6"
-                    />
-                  </svg>
+                  <p className="font-normal text-[13px] leading-[18px] line-clamp-1 text-[#555555]">
+                    {randomDescription}
+                  </p>
                 </div>
-                <img src={Avatar} alt="userAvatar" />
+                <div className="flex justify-between items-center">
+                  <div className="flex h-[20px]  mr-[4px]">
+                    <span className="flex justify-center items-center gap-[8px] bg-[#B233A6] px-[6px] py-[2px] mr-[4px] rounded-[4px] text-white font-semibold text-xs">
+                      {randomTagOne}
+                    </span>
+                    <div className="flex justify-center items-center h-[20px] after:border-[10px] after:border-[solid] after:border-[transparent] after:border-l-[10px] after:border-l-[#EBEEF6]">
+                      <span className="px-[6px] py-[2px] bg-[#EBEEF6] rounded-l-[4px]  text-[#616C82] font-semibold text-xs overflow-hidden">
+                        {randomTagTwo}
+                      </span>
+                    </div>
+                  </div>
+                  <img src={Avatar} alt="userAvatar" />
+                </div>
               </div>
             </li>
           );
--- src/components/ToDoPage.tsx
+++ src/components/ToDoPage.tsx
@@ -7,29 +7,37 @@ import { IToDoItem } from "../types";
 
 export const ToDoPage: React.FC = () => {
   const [toDos, setToDos] = useState<IToDoItem[] | null>(null);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
-    axios
-      .get("https://jsonplaceholder.typicode.com/todos")
-      .then((res) => setToDos(res.data));
+    axios.get("https://jsonplaceholder.typicode.com/todos").then((res) => {
+      setToDos(res.data);
+      setIsLoading(false);
+    });
   }, []);
 
   return (
     <div className="flex flex-col items-center mt-[92px]">
-      <div className="w-[263px] overflow-scroll h-[716px] gap-[8px] flex flex-col">
-        <div className="flex justify-between items-center px-[4px] py-0">
-          <h2 className="font-semibold text-sm leading-5 text-[#50B810]">
-            Today
-          </h2>
-          <div className="flex justify-between gap-[8px]">
-            <img src={PlusSign} alt="plus sign" />
-            <span className="px-[6px] py-[2px] border border-[#7B8AAB] rounded-[4px] border-[1px] text-[#555555] font-semibold text-xs">
-              {toDos ? toDos.length : 0}
-            </span>
+      {isLoading ? (
+        <h1>Loading...</h1>
+      ) : (
+        <div className="w-[263px] h-[716px] gap-[8px] flex flex-col">
+          <div className="flex justify-between items-center px-[4px] py-0">
+            <h2 className="font-semibold text-sm leading-5 text-[#50B810]">
+              Today
+            </h2>
+            <div className="flex justify-between gap-[8px]">
+              <button disabled>
+                <img src={PlusSign} alt="plus sign" className="h-full" />
+              </button>
+              <span className="px-[6px] py-[2px] border border-[#7B8AAB] rounded-[4px] border-[1px] text-[#555555] font-semibold text-xs">
+                {toDos ? toDos.length : 0}
+              </span>
+            </div>
           </div>
+          <ToDoList toDos={toDos} />
         </div>
-        <ToDoList toDos={toDos} />
-      </div>
+      )}
     </div>
   );
 };
